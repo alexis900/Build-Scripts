3d003c76d997cfaf07e45174ac1311e5bf1d1c86
Dave <davemartinfsu@gmail.com>
2014-07-28 08:52:09 -0400

    update merge.sh

--- merge.sh
+++ merge.sh
@@ -3,23 +3,30 @@
 tput setf 6; echo "Merging frameworks/av..."; tput setf 7
 echo " "
 cd ~/cm11/frameworks/av
-#git remote add upstream https://github.com/kumajaya/android_frameworks_av
-#git fetch upstream
+git remote add upstream https://github.com/kumajaya/android_frameworks_av
+git fetch upstream
 #git checkout cm-11.0
 #git merge -m "merge kumajaya commits" upstream/cm-11.0
-git cherry-pick ee6fe4fb247b1bafb438f803ee3a99cf09af7ab1
+git cherry-pick 6efb57fcdafb139559ab93b751fcd332ca296a3b
 echo "Done"
 echo " "
 tput setf 6; echo "Merging system/vold..."; tput setf 7
 echo " "
 cd ~/cm11/system/vold
-#git remote add upstream https://github.com/kumajaya/android_system_vold
-#git fetch upstream
+git remote add upstream https://github.com/kumajaya/android_system_vold
+git fetch upstream
 #git checkout cm-11.0
 #git merge -m "merge kumajaya commits" upstream/cm-11.0
-git cherry-pick c5df0ace9b1d10ba6e6f1b73e838ffb7ad22ff1d
-git cherry-pick b4a568a9cd6ab78035671b573ddcf3031486f80c
-git cherry-pick 89cea1628ee38216042832d9ae4d0957a23d85c9
+git cherry-pick ded69dfcdd3faa8ab8998610cc12c4e1d7333690
+git cherry-pick e147e2ba270eff2418cedc5e3ad195b00a72b34f
+git cherry-pick bd795445b6e4f0ed2aeebfa0c2ed25fe21000ae9
+
+#temporary until merged
+tput setf 6; echo "Merging frameworks/base..."; tput setf 7
+echo " "
+cd ~/cm11/frameworks/base
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_base refs/changes/21/68621/5 && git checkout FETCH_HEAD
+
 echo "Done"
 echo " "
 done
