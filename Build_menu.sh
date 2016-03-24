ec154c72f3ce65cbc4c67c790df73e40096bfc12
Dave Martin <davemartinfsu@gmail.com>
2016-03-24 15:56:59 -0400

    add ccache size to menu and update colors

--- Build_menu.sh
+++ Build_menu.sh
@@ -1,14 +1,18 @@
 export PATH=${PATH}:~/bin
+
 while :
 do
   clear
-
+#~/cm13/prebuilts/misc/linux-x86/ccache/ccache -s > ~/ccache_stats1
+stats=$(~/cm13/prebuilts/misc/linux-x86/ccache/ccache -s)
+ccachesize=$(echo "$stats" | grep -m 1 'cache size')
+size=$(echo "$ccachesize" | cut -b 37-48 )
   echo 
   echo "============================================================"
   echo "    gr8nole's Build Menu            >>>>--------;;;----->"
   echo "============================================================"
-  echo 
-  echo "> MAIN MENU"
+  echo
+  echo -n "  MAIN MENU";echo "                     \033[38;5;46mccache=$size\033[39m"
   echo
 echo " 1. - AICP - Build All-NoSync	1b.- Bliss - Build All-NoSync	"
 echo " 2. - AICP - Build T310   	2b.- Bliss - Build T310   	"
@@ -24,7 +28,7 @@ echo " 11.- Build T311 cm13		ta.- AICP terminal"
 echo " 12.- Build T315 cm13		tb.- Bliss terminal"
 echo " 13.- Build T311_315 cm13	tc.- CM13 terminal"
 echo " "
-echo " r.- Reset Colors		uu.- Check ccache"
+echo " r.- Reset Colors		uu.- Check all ccache stats"
 echo " x.- EXIT "
   echo 
   echo -n "Enter option: "
@@ -32,34 +36,34 @@ echo " x.- EXIT "
 if [ "$?" != "1" ]
   then
     case $opt in
-      1) tput sgr0;~/scripts/clobber.sh;~/scripts/build_aicp_nosync.sh;tput setf 0; tput setb 7;;
-      2) tput sgr0;~/scripts/clobber.sh;~/scripts/build_aicp_T310.sh;tput setf 0; tput setb 7;;
-      3) tput sgr0;~/scripts/clobber.sh;~/scripts/build_aicp_T311_315.sh;tput setf 0; tput setb 7;;
-      4) tput sgr0;~/scripts/clobber.sh;~/scripts/make_T310_recovery.sh;tput setf 0; tput setb 7;;
-      5) tput sgr0;~/scripts/clobber.sh;~/scripts/make_T311_recovery.sh;tput setf 0; tput setb 7;;
-      6) tput sgr0;~/scripts/clobber.sh;~/scripts/make_T315_recovery.sh;tput setf 0; tput setb 7;;
+      1) ~/scripts/clobber.sh;~/scripts/build_aicp_nosync.sh;tput setaf 3;;
+      2) ~/scripts/clobber.sh;~/scripts/build_aicp_T310.sh;tput setaf 3;;
+      3) ~/scripts/clobber.sh;~/scripts/build_aicp_T311_315.sh;tput setaf 3;;
+      4) ~/scripts/clobber.sh;~/scripts/make_T310_recovery.sh;tput setaf 3;;
+      5) ~/scripts/clobber.sh;~/scripts/make_T311_recovery.sh;tput setaf 3;;
+      6) ~/scripts/clobber.sh;~/scripts/make_T315_recovery.sh;tput setaf 3;;
       7) ;;
-      8) tput sgr0;~/scripts/clobber.sh;~/scripts/build_nosync.sh;tput setf 0; tput setb 7;;
-      9) tput sgr0;~/scripts/clobber.sh;~/scripts/build.sh;tput setf 0; tput setb 7;;
-      10) tput sgr0;~/scripts/clobber.sh;~/scripts/build_T310.sh;tput setf 0; tput setb 7;;
-      11) tput sgr0;~/scripts/clobber.sh;~/scripts/build_T311.sh;tput setf 0; tput setb 7;;
-      12) tput sgr0;~/scripts/clobber.sh;~/scripts/build_T315.sh;tput setf 0; tput setb 7;;
-      13) tput sgr0;~/scripts/clobber.sh;~/scripts/build_T311_315.sh;tput setf 0; tput setb 7;;
-      1b) tput sgr0;~/scripts/clobber.sh;~/scripts/build_bliss_nosync.sh;tput setf 0; tput setb 7;;
-      2b) tput sgr0;~/scripts/clobber.sh;~/scripts/build_bliss_T310.sh;tput setf 0; tput setb 7;;
-      3b) tput sgr0;~/scripts/clobber.sh;~/scripts/build_bliss_T311_315.sh;tput setf 0; tput setb 7;;
+      8) ~/scripts/clobber.sh;~/scripts/build_nosync.sh;tput setaf 3;;
+      9) ~/scripts/clobber.sh;~/scripts/build.sh;tput setaf 3;;
+      10) ~/scripts/clobber.sh;~/scripts/build_T310.sh;tput setaf 3;;
+      11) ~/scripts/clobber.sh;~/scripts/build_T311.sh;tput setaf 3;;
+      12) ~/scripts/clobber.sh;~/scripts/build_T315.sh;tput setaf 3;;
+      13) ~/scripts/clobber.sh;~/scripts/build_T311_315.sh;tput setaf 3;;
+      1b) ~/scripts/clobber.sh;~/scripts/build_bliss_nosync.sh;tput setaf 3;;
+      2b) ~/scripts/clobber.sh;~/scripts/build_bliss_T310.sh;tput setaf 3;;
+      3b) ~/scripts/clobber.sh;~/scripts/build_bliss_T311_315.sh;tput setaf 3;;
       4a) ;;
-      5a) tput sgr0; ~/scripts/sync_only_aicp.sh;tput setf 0; tput setb 7;;
-      5b) tput sgr0; ~/scripts/sync_only_bliss.sh;tput setf 0; tput setb 7;;
-      5c) tput sgr0; ~/scripts/sync_only.sh;tput setf 0; tput setb 7;;
+      5a)  ~/scripts/sync_only_aicp.sh;tput setaf 3;;
+      5b)  ~/scripts/sync_only_bliss.sh;tput setaf 3;;
+      5c)  ~/scripts/sync_only.sh;tput setaf 3;;
       g)  ;;
-      cc) tput sgr0;~/scripts/clobber.sh;tput setf 0; tput setb 7;;
+      cc) ~/scripts/clobber.sh;tput setaf 3;;
       m)  ;;
-      ta) tput sgr0;gnome-terminal  --working-directory=/home/dave/aicp;tput setf 0; tput setb 7;;
-      tb) tput sgr0;gnome-terminal  --working-directory=/home/dave/bliss;tput setf 0; tput setb 7;;
-      tc) tput sgr0;gnome-terminal  --working-directory=/home/dave/cm13;tput setf 0; tput setb 7;;
+      ta) gnome-terminal  --working-directory=/home/dave/aicp;tput setaf 3;;
+      tb) gnome-terminal  --working-directory=/home/dave/bliss;tput setaf 3;;
+      tc) gnome-terminal  --working-directory=/home/dave/cm13;tput setaf 3;;
       r) tput sgr0;;
-      uu) tput sgr0;~/cm13/prebuilts/misc/linux-x86/ccache/ccache -s;tput setf 0; tput setb 7;;
+      uu) ~/cm13/prebuilts/misc/linux-x86/ccache/ccache -s;tput setaf 3;;
       x) exit;;
     esac
   fi
