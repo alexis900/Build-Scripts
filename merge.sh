068b0a767a303b6ba70829f1bb113ed76e53864f
gr8nole <davemartinfsu@gmail.com>
2016-05-23 14:52:06 -0400

    stride alignment cheery-pick no longer needed
    It was finally merged in CM

--- merge.sh
+++ merge.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
 echo " "
-echo -e "\E[1;36mCherry-picking $SOURCE frameworks/av..."; tput sgr0
-echo " "
-echo -e "\E[1;36mMoving to $SOURCE frameworks/av directory..."; tput sgr0
-echo " "
-cd ~/$SOURCE/frameworks/av
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_av refs/changes/30/137230/1 && git cherry-pick FETCH_HEAD
+#echo -e "\E[1;36mCherry-picking $SOURCE frameworks/av..."; tput sgr0
+#echo " "
+#echo -e "\E[1;36mMoving to $SOURCE frameworks/av directory..."; tput sgr0
+#echo " "
+#cd ~/$SOURCE/frameworks/av
+#git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_av refs/changes/30/137230/1 && git cherry-pick FETCH_HEAD
 
-
+echo -e "\E[1;36mNothing to merge or pick..."; tput sgr0
 
 
 echo " "
