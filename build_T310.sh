b29b4aa551422c4d7cfbb93ef0155c36a21caafb
gr8nole <davemartinfsu@gmail.com>
2016-06-14 07:44:53 -0400

    disable incremental ota build
    because it rarely ever flashes correctly

--- build_T310.sh
+++ build_T310.sh
@@ -48,26 +48,26 @@ if [ -f ~/$SOURCE/out/target/product/$DEVICE/${SOURCE:n:2}*UNOFFICIAL*.zip ]; th
 fi
 shopt -u nocaseglob
 
-echo -e "\E[1;32mMaking incremental OTA update..."; tput sgr0
-echo " "
-
-newtarget=$(find ~/$SOURCE/out/target/product/$DEVICE/obj/PACKAGING/target_files_intermediates/*.zip -maxdepth 1 -mtime -1)
-./build/tools/releasetools/ota_from_target_files -i /home/dave/Builds/ota_base/${SOURCE:n:2}*_$DEVICE-target_files-*.zip $newtarget ~/Builds/$(date +"%m-%d-%Y")/$SOURCE-$DEVICE-inc_update_$(date +"%Y%m%d").zip
-
-   
-if [ -f ~/Builds/$(date +"%m-%d-%Y")/$SOURCE-$DEVICE-inc_update_$(date +"%Y%m%d").zip ]; then
-  if [ ! -d ~/Builds/ota_base/old ]; then
-      mkdir ~/Builds/ota_base/old
-  fi
- mv ~/Builds/ota_base/${SOURCE:n:2}*_$DEVICE-target_files-*.zip ~/Builds/ota_base/old/
- cp ~/$SOURCE/out/target/product/$DEVICE/obj/PACKAGING/target_files_intermediates/${SOURCE:n:2}*_$DEVICE-target_files-*.zip ~/Builds/ota_base/
- PASS=$(< ~/bin/pass)
- echo -e "\E[1;32mUploading $DEVICE $SOURCE zips..."; tput sgr0
- echo " "
- ls ~/Builds/$(date +"%m-%d-%Y")/*${SOURCE:1:1}*$DEVICE*.zip > files.txt
- mapfile -t myArray < files.txt
- curl -T ${myArray[0]} ftp://uploads.androidfilehost.com --user gr8nole:$PASS
- curl -T ${myArray[1]} ftp://uploads.androidfilehost.com --user gr8nole:$PASS
- rm files.txt
-
-fi
+#echo -e "\E[1;32mMaking incremental OTA update..."; tput sgr0
+#echo " "
+#
+#newtarget=$(find ~/$SOURCE/out/target/product/$DEVICE/obj/PACKAGING/target_files_intermediates/*.zip -maxdepth 1 -mtime -1)
+#./build/tools/releasetools/ota_from_target_files -i /home/dave/Builds/ota_base/${SOURCE:n:2}*_$DEVICE-target_files-*.zip $newtarget ~/Builds/$(date +"%m-%d-%Y")/$SOURCE-$DEVICE-inc_update_$(date +"%Y%m%d").zip
+#
+#   
+#if [ -f ~/Builds/$(date +"%m-%d-%Y")/$SOURCE-$DEVICE-inc_update_$(date +"%Y%m%d").zip ]; then
+#  if [ ! -d ~/Builds/ota_base/old ]; then
+#      mkdir ~/Builds/ota_base/old
+#  fi
+# mv ~/Builds/ota_base/${SOURCE:n:2}*_$DEVICE-target_files-*.zip ~/Builds/ota_base/old/
+# cp ~/$SOURCE/out/target/product/$DEVICE/obj/PACKAGING/target_files_intermediates/${SOURCE:n:2}*_$DEVICE-target_files-*.zip ~/Builds/ota_base/
+# PASS=$(< ~/bin/pass)
+# echo -e "\E[1;32mUploading $DEVICE $SOURCE zips..."; tput sgr0
+# echo " "
+# ls ~/Builds/$(date +"%m-%d-%Y")/*${SOURCE:1:1}*$DEVICE*.zip > files.txt
+# mapfile -t myArray < files.txt
+# curl -T ${myArray[0]} ftp://uploads.androidfilehost.com --user gr8nole:$PASS
+# curl -T ${myArray[1]} ftp://uploads.androidfilehost.com --user gr8nole:$PASS
+# rm files.txt
+#
+#fi
